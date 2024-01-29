3dc728b3a175f9e94fa07615428bc0e67932fd65
BEST <deliteser112@gmail.com>
2024-01-29 20:24:36 +0100

    Create index.js
    Co-Authored-By: bestdev112 <158070421+bestdev112@users.noreply.github.com>
    Co-Authored-By: Origindev <130565765+origindev0719@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,35 @@
+import 'react/jsx-runtime';
+
+import ConnectedWidget from './src';
+import {
+  addUserMessage,
+  addResponseMessage,
+  addLinkSnippet,
+  renderCustomComponent,
+  toggleWidget,
+  toggleInputDisabled,
+  toggleMsgLoader,
+  dropMessages,
+  isWidgetOpened,
+  setQuickButtons,
+  deleteMessages,
+  markAllAsRead,
+  setBadgeCount
+} from './src/store/dispatcher';
+
+export {
+  ConnectedWidget as Widget,
+  addUserMessage,
+  addResponseMessage,
+  addLinkSnippet,
+  renderCustomComponent,
+  toggleWidget,
+  toggleInputDisabled,
+  toggleMsgLoader,
+  dropMessages,
+  isWidgetOpened,
+  setQuickButtons,
+  deleteMessages,
+  markAllAsRead,
+  setBadgeCount
+};
