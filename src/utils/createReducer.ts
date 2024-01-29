3da6b535b48cf2144c820e4a5679a825e04ae054
BEST <deliteser112@gmail.com>
2024-01-29 20:21:42 +0100

    some features removed

--- src/utils/createReducer.ts
+++
@@ -1,5 +0,0 @@
-export const createReducer = <S>(
-  reducer: { [key: string]: Function },
-  state: S,
-  action: { [key: string]: any }
-) => (reducer[action.type] ? reducer[action.type](state, action) : state);
--- src/utils/types.ts
+++
@@ -1,3 +0,0 @@
-export type Nullable<T> = T | null;
-
-export type AnyFunction = (...args: any[]) => any;
