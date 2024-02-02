c7f9339a77eef3948f47e153f8c87a253c9c5b2b
BEST <deliteser112@gmail.com>
2024-02-02 06:06:40 +0100

    Create index.tsx
    Co-Authored-By: Origindev <130565765+origindev0719@users.noreply.github.com>

---
+++ src/components/Widget/components/Launcher/index.tsx
@@ -0,0 +1,46 @@
+import { useSelector, useDispatch } from 'react-redux';
+import cn from 'classnames';
+
+import Badge from './components/Badge';
+import { GlobalState } from '../../../../store/types';
+import { setBadgeCount } from '../../../../store/actions';
+
+import './style.scss';
+
+const openLauncher = require('../../../../../assets/launcher_button.svg') as string;
+const close = require('../../../../../assets/clear-button.svg') as string;
+
+type Props = {
+  toggle: () => void;
+  chatId: string;
+  openLabel: string;
+  closeLabel: string;
+  closeImg: string;
+  openImg: string;
+  showBadge?: boolean;
+}
+
+function Launcher({ toggle, chatId, openImg, closeImg, openLabel, closeLabel, showBadge }: Props) {
+  const dispatch = useDispatch();
+  const { showChat, badgeCount } = useSelector((state: GlobalState) => ({
+    showChat: state.behavior.showChat,
+    badgeCount: state.messages.badgeCount
+  }));
+
+  const toggleChat = () => {
+    toggle();
+    if (!showChat) dispatch(setBadgeCount(0));
+  }
+
+  return (
+    <button type="button" className={cn('rcw-launcher', { 'rcw-hide-sm': showChat })} onClick={toggleChat} aria-controls={chatId}>
+      {!showChat && showBadge && <Badge badge={badgeCount} />}
+      {showChat ?
+        <img src={closeImg || close} className="rcw-close-launcher" alt={openLabel} /> :
+        <img src={openImg || openLauncher} className="rcw-open-launcher" alt={closeLabel} />
+      }
+    </button>
+  );
+}
+
+export default Launcher;
